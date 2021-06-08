212b51171b0a031b2702a81fd8b84eaebba96cba
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2021-06-07 20:14:08 -0400

    Add files via upload

---
+++ 1/Main.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ *
+ * @author 100661873
+ */
+
+
+public class Main {
+    
+    public static void main (String[] args) throws IOException {
+        //hold the fibonacci array from 0 to 60
+        long fib [] = new long [61];
+        //base case for the sequence
+        fib [0] = 0;
+        fib [1] = 1;
+        //load the array with the rest of the sequence
+        for (int i = 2; i < 61; i++) {
+            fib[i] = fib [i-1] +fib [i-2];
+        }
+        //retrieve number of ints to be read
+        Scanner s = new Scanner(System.in);
+        int T = s.nextInt();
+        //repeat T times
+        for (int i = 0; i < T; i++) { 
+            //retrieve number from user
+            int N = s.nextInt();
+            //print if number is within boundaries for this fib array
+            if (N <= 60 && N >= 0)
+                System.out.println ("Fib(" + N + ") = " + fib[N]);
+        }
+       
+    } 
+}
